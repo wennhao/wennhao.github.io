19b0d7e1ead77423f5b2a81cdc74846e772e187c
wen <1028905@hr.nl>
2023-10-06 03:11:23 +0200

    changes1

--- script.js
+++
@@ -1,7 +0,0 @@
-const galaxy = document.querySelector('.galaxy');
-
-galaxy.addEventListener('mousemove', (e) => {
-    const moveX = (e.clientX / window.innerWidth) * 100;
-    const moveY = (e.clientY / window.innerHeight) * 100;
-    galaxy.style.transform = `translate(-${moveX}%, -${moveY}%)`;
-});
