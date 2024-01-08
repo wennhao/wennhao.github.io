030f079821e863e53ef700c18284b051ce57c69f
wen <1028905@hr.nl>
2024-01-08 22:39:15 +0100

    trying to add a menu

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Laad de startpagina in als standaardinhoud
-    loadPage('home.html');
+function loadMenu(menuFile) {
+    const header = document.querySelector('header');
 
-    // Voeg klikgebeurtenissen toe aan menu-items
-    var menuItems = document.querySelectorAll('#topbar a');
-    
-    menuItems.forEach(function (item) {
-        item.addEventListener('click', function (event) {
-            event.preventDefault(); // Voorkom standaardgedrag van de link
-            var pageUrl = item.getAttribute('href');
-            loadPage(pageUrl);
-        });
-    });
+    // Remove existing scripts to avoid conflicts
+    const existingScripts = header.querySelectorAll('script');
+    existingScripts.forEach(script => script.remove());
 
-    // Functie om pagina-inhoud dynamisch te laden
-    function loadPage(url) {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                document.getElementById('content').innerHTML = xhr.responseText;
-            }
-        };
-        xhr.open('GET', url, true);
-        xhr.send();
-    }
-});
+    // Load the new menu using a script element
+    const script = document.createElement('script');
+    script.src = menuFile;
+    script.type = 'text/javascript';
+    header.appendChild(script);
+}
