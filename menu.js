2b9580bd66ac53264271ae496b2718a91fed3333
wen <1028905@hr.nl>
2024-01-08 23:06:28 +0100

    trying to fix the double menu bar issue

--- menu.js
+++ menu.js
@@ -10,7 +10,11 @@ function loadPage(page) {
         var contentStart = data.indexOf('<body>') + 6;
         var contentEnd = data.indexOf('</body>');
         var content = data.substring(contentStart, contentEnd);
-        
+  
+        // Clear existing content
+        document.getElementById('content').innerHTML = '';
+  
+        // Add new content
         document.getElementById('content').innerHTML = content;
       });
   }
