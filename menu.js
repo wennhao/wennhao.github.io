8eddc9709d6130ee86abd725a150ccf586f6accb
wen <1028905@hr.nl>
2024-01-08 22:55:36 +0100

    modified files

--- menu.js
+++ menu.js
@@ -1,4 +1,7 @@
 function loadPage(page) {
+    // Prevent default behavior of anchor tag
+    event.preventDefault();
+  
     // Load content of the selected page into the 'content' div
     fetch(page)
       .then(response => response.text())
